3fac1f596a572cf337be5eff9d1e242bb40e1714
Ali SAAD <ali.saad@mines-paristech.fr>
2014-06-03 15:55:35 +0200

    Finalized the Tmesh.View function for 2D and 3D for bothe engines mpl and myv Added also a cimpy.view function
    Signed-off-by: Ali SAAD <ali.saad@mines-paristech.fr>

--- cimpy.py
+++ cimpy.py
@@ -4,12 +4,20 @@
 # import sys
 import numpy as np
 import matplotlib.pyplot as plt
+import mayavi.mlab as ml # to use mayavi mlab  scripting tools and rendering
+from mpl_toolkits.mplot3d import Axes3D #to use Scatter/Scatter 3D
+from tvtk.api import tvtk
+
+
 # import vtk
 
 # All returned arrays are cast into either numpy or numarray arrays
 #arr=numpy.array
 
 # ALI: "self" in python is equivalent to "this" in C++
+
+# TODO : implement class create_grid(filename) to link with Struct_mesh_generator.cpp ==> possible use of ctypes wrapping ??
+
 class Tmesh:
   """Mesh object containing nodes and elements, as well connectivity and dimension information"""
   
@@ -17,12 +25,11 @@ class Tmesh:
     """Creates a Tmesh object by reading the specified file."""
     
     if filename is None:
-      print "The mesh filename is not specified. Please input a valid mesh name with a .t extension" 
+      print "CIMPY:: The mesh filename is not specified. Please input a valid mesh name with a .t extension" 
       self.name = "Maillage1" 
     else:
-      # self.gridreader = None
       if filename[-2:].lower() == ".t":
-        print "Mesh filename is valid and ready for reading ..."
+        print "CIMPY:: Mesh filename is valid and ready for reading ..."
         self.name = filename.split()[0]
 #        self.point_array = [] # TODO: obsolete ... replaced by dictionary
         self.nodes_dict = {} # or =dict()        
@@ -31,22 +38,23 @@ class Tmesh:
 #        self.element_array = [] # TODO: obsolete ... replaced by dictionary
         self.Parse_input_file(filename)
       elif filename[-4:].lower() == ".msh":
-        print "Coming soon: convert " + filename + " to a cimlib compatible mesh format (Tmesh) ? "
+        print "CIMPY:: Coming soon: convert " + filename + " to a cimlib compatible mesh format (Tmesh) ? "
 	  # TODO link with gmsh2mtc.py
       else:
         raise Exception("ERROR: The extension of " +  filename + "  filename is not recognised")
       
       
   def unittest(self):
-      """ check if constructor is running """
+      """ Not implemented yet """
       print "My name is: " + self.name
       
-      
+  
   def Parse_input_file(self, filename):
       """ takes a .t mesh filename and read it into a Tmesh datastructure """
       f = open(filename)
       
       header = f.readline().strip().split()
+      # TODO: add condition: if len(header) != 4 then ...
       self.nbNodes, self.dimNode,self.nbElements, self.dimElement = header
       
       self.nbNodes = int(self.nbNodes)
@@ -82,12 +90,35 @@ class Tmesh:
       f.close()
       
       
-  def View(self, **kwargs):
-      """ Plots the mesh in a figure. Supports only 2D mesh for now  """
-      #TODO: check Poly3DCollection(verts, *args, **kwargs) for 3D
-
-      """ a dict constructor is necessary so that the class's member
-      dictionary wont be affected when fictitious triangles are suppressed"""
+  def View(self, engine = "mpl" , **kwargs):
+      """ Plots the mesh in a figure. Supports only 2D and 3D mesh
+      @param engine = 'mpl'  to use a matplotlib as 2D and 3D scatter plotting backend
+                    = 'myv' (default) to use Mayavi for 2D and 3D mesh rendering (using tvtk)
+                    
+      @param **kwargs
+              if 'engine' argument is set to "mpl" then Matplotlib's line keywords are used:
+                  
+                linestyle or ls	: '-' , '--' , '-.' ...
+                linewidth or lw	float value in points
+                marker: 'x' , 'o' , 'v' ...
+                color: 'r' (red), 'k' (black) , 'b' (blue) ...
+                [check url: http://matplotlib.org/1.3.1/api/artist_api.html#module-matplotlib.lines]
+                  
+              
+              if 'engine' argument is set to "myv" then Matplotlib's keywords are used:
+                  
+                color:	the color of the vtk object. Overides the colormap, if any, when specified. This is specified as a triplet of float ranging from 0 to 1, eg (1, 1, 1) for white.
+                colormap:	type of colormap to use.
+                extent:	[xmin, xmax, ymin, ymax, zmin, zmax] Default is the x, y, z arrays extent. Use this to change the extent of the object created.
+      """
+      
+      dim = self.dimNode 
+      nb_points_real = self.GetNumberOfPoints()
+      nb_points = nb_points_real + 1 # the fictitious node of index 0 and coordinates [0,0]
+      
+      
+      ### a dict constructor is necessary so that the class's member
+      ### dictionary wont be affected when fictitious triangles are suppressed
       real_elements_dict = dict(self.elements_dict)
 
       # the 'enumerate' function is really nice !
@@ -96,21 +127,67 @@ class Tmesh:
          # i+1 because enumerate starts at 0 while my dict starts at 1 
      
       # The lists have to be cast into numpy arrays before using the TRI plot
-      nodes_coord = np.asarray(self.nodes_dict.values())
-      triangles = np.asarray(real_elements_dict.values())
+      points = np.asarray(self.nodes_dict.values())
+      simplexes = np.asarray(real_elements_dict.values())
      
       # Extracting columns
-      x = nodes_coord[:,0]
-      y = nodes_coord[:,1]
-      if self.dimNode == 3:
-          z = nodes_coord[:,2]
-           
-      plt.figure()
-      plt.gca().set_aspect('equal')
-      plt.triplot(x, y, triangles, **kwargs)
-      plt.title('Mesh')
+      x = points[:,0]
+      y = points[:,1]
+      
+      if dim == 2:
+          # points_2d is used in mayavi rendering
+          points_2d = np.column_stack([points, np.zeros((nb_points,1))])
+      elif dim == 3:  
+          # z is used in matplotlib rendering
+          z = points[:,2]
+          # points_3d is used in mayavi rendering
+          points_3d = points
     
-      plt.show() 
+    
+      if engine == "mpl":
+          
+          if dim == 2:
+              plt.figure()
+              plt.gca().set_aspect('equal')
+              plt.triplot(x, y, simplexes, **kwargs)
+              plt.title('Mesh')
+              plt.show() 
+              
+          elif  dim == 3:
+              # TODO: add line connection between scattered points
+              fig = plt.figure()
+              ax = fig.add_subplot(111, projection='3d', aspect ='equal')
+              ax.scatter3D(x, y, z, **kwargs)
+              ax.set_xlabel('X Label')
+              ax.set_ylabel('Y Label')
+              ax.set_zlabel('Z Label')
+              plt.axis()
+              plt.show()
+      
+      elif engine == "myv":
+              
+          if dim == 2:
+              #this way works
+#              z = np.zeros_like(x)
+#              triangles = []
+#              for trigl in real_elements_dict.values(): triangles.append(tuple(trigl))
+#              ml.triangular_mesh(x, y, z, triangles, representation = 'wireframe')  
+              
+              #another way
+               simplex_type = 5  # = 10 for tetrahedron 
+               ug = tvtk.UnstructuredGrid(points= points_2d)
+              
+          if dim == 3:
+              simplex_type = tvtk.Tetra().cell_type  # = 10 for tetrahedron 
+              ug = tvtk.UnstructuredGrid(points= points_3d)
+              
+          
+          ug.set_cells(simplex_type, simplexes) 
+          fig = ml.figure(bgcolor=(1, 1, 1), fgcolor=(0, 0, 0), figure=ug.class_name[3:])
+          surf = ml.pipeline.surface(ug, opacity=0.01)
+          RGB= (0,0,0.5)
+          #edge_color = tuple(i/255 for i in RGB)
+          ml.pipeline.surface(ml.pipeline.extract_edges(surf), color= RGB )
       
       
   ### Getters ###    
@@ -139,7 +216,7 @@ class Tmesh:
       """ returns an array containing the nodes coordinates """
      
       if filename is None:
-         print "The output filename is not specified ... aborting"
+         print "CIMPY:: The output filename is not specified ... aborting"
       else:
           g = open(filename, "w")
           for id in range(1,self.nbNodes+1): 
@@ -156,7 +233,7 @@ class Tmesh:
       """ returns an array containing the triangles / connectivity"""
       
       if filename is None:
-         print "The output filename is not specified ... aborting"
+         print "CIMPY:: The output filename is not specified ... aborting"
       else:
           g = open(filename, "w")
           for id in range(1,self.nbElements+1): 
@@ -229,7 +306,7 @@ class Telement:
     elif self.dimension == 4:
         id1, id2, id3, id4 = self.connectivity  
     else:
-        raise Exception ("Error Telement dimension" +str(self.dimension) + ": an element's dimension should be either 3 (triangle in 2D) or 4")
+        raise Exception ("CIMPY:: Error Telement dimension" +str(self.dimension) + ": an element's dimension should be either 3 (triangle in 2D) or 4")
      
     nodes_dic = input_mesh.nodes_dict
     self.node1 = nodes_dic[id1]
@@ -244,4 +321,11 @@ class Telement:
      elif dimension == 3:
             return str(self.connectivity[0]) + str("\t") + str(self.connectivity[1]) + str("\t") + str(self.connectivity[2]) +  + str("\t") + str(self.connectivity[3]) + str("\n")
 
+#==================================================
+def view(filename, engine= "mpl"):
+    """ a cimpy function to visualize a mesh using cimpy's Tmesh class """
+    """   argument: filename of the mesh
+    """
+    mesh = Tmesh(filename)
+    mesh.View(engine)
 #==================================================
--- test.py
+++ test.py
@@ -1,63 +1,60 @@
-print "ffffffffffffffuuuuucckkk"
+
+print "\n\n \t LAUNCHING TEST SCRIPT :: ALI SAAD 2014 \n\n"
 
 
 import cimpy as cim
 import matplotlib.pyplot as plt
 import numpy as np
-from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+
+import mayavi.mlab as ml
 from mpl_toolkits.mplot3d import Axes3D
 
 
-mesh = cim.Tmesh("para_mesh.t") # para_mesh.t  # CET_petit # mesh3d
-
-mesh.View(color='r',
-          linewidth=1.0,
-          marker = 'x'
-          )
 
 
-elements = mesh.elements_dict
+mesh = cim.Tmesh("CET_petit.t") # para_mesh.t  # CET_petit # mesh3d
+#mesh.View("mpl")
+#mesh.View("myv")
 
 
-############# 3D  VIEW function MAYAVI ##############
-##real_elements_dict = dict(mesh.elements_dict)
-##
-### the 'enumerate' function is really nice !
-##for i,connec_list in enumerate(real_elements_dict.values()):
-##    if 0 in connec_list : del real_elements_dict[i+1]
-##    # i+1 because enumerate starts at 0 while my dict starts at 1
-##
-### The lists have to be cast into numpy arrays before using the TRI plot
-##nodes_coord = np.asarray(mesh.nodes_dict.values())
-##triangles = np.asarray(real_elements_dict.values())
-##
-### Extracting columns
-##x = nodes_coord[:,0]
-##y = nodes_coord[:,1]
-##if mesh.dimNode == 3:
-##     z = nodes_coord[:,2]
-##
-##fig = plt.figure()
-##ax = fig.add_subplot(111, projection='3d')
-##
-##ax.scatter3D(x, y, z, c='r', marker='o')
-##
-##ax.set_xlabel('X Label')
-##ax.set_ylabel('Y Label')
-##ax.set_zlabel('Z Label')
-##
-##plt.show()
-##
-##
 
-############# 3D  VIEW function SCATTER3D :: use with mesh3d ##############
-
+############# 3D  VIEW function MAYAVI :: use with mesh3d.t ##############
+#
+#x1 = []
+#y1 = []
+##z1 = []
+#for couple in mesh.nodes_dict.values():
+#        x1.append(couple[0])
+#        y1.append(couple[1])
+##        z1.append(couple[1])
+#        
+#x1 = np.array(x1)
+#y1 = np.array(y1)
+#z1 = np.zeros_like(x1)
+#triangles = []
+#
 #real_elements_dict = dict(mesh.elements_dict)
 #
 ## the 'enumerate' function is really nice !
 #for i,connec_list in enumerate(real_elements_dict.values()):
 #    if 0 in connec_list : del real_elements_dict[i+1]
 #    # i+1 because enumerate starts at 0 while my dict starts at 1
+#
+#
+#for trigl in real_elements_dict.values():
+#    triangles.append(tuple(trigl))
+#        
+#ml.triangular_mesh(x1, y1, z1, triangles, 
+#                   representation = 'wireframe')   
+
+############# 3D  VIEW function SCATTER3D :: use with mesh3d.t ##############
+
+#real_elements_dict = dict(mesh.elements_dict)
+#
+## the 'enumerate' function is really nice !
+#for i,connec_list in enumerate(real_elements_dict.values()):
+#   if 0 in connec_list : del real_elements_dict[i+1]
+#   # i+1 because enumerate starts at 0 while my dict starts at 1
 #
 ## The lists have to be cast into numpy arrays before using the TRI plot
 #nodes_coord = np.asarray(mesh.nodes_dict.values())
@@ -71,9 +68,9 @@ elements = mesh.elements_dict
 #
 #fig = plt.figure()
 #ax = fig.add_subplot(111, projection='3d')
-#
-#ax.scatter3D(x, y, z, c='r', marker='o')
-#
+##
+#ax.scatter3D(x, y, z, c='r', marker='x')
+##
 #ax.set_xlabel('X Label')
 #ax.set_ylabel('Y Label')
 #ax.set_zlabel('Z Label')
